49ae562aa9965a66a2af121a546eef375765dcd2
Emily Kauffman <kauffmanes@gmail.com>
2019-07-16 19:54:20 -0400

    making it a singleton

--- SingletonShapeFactory.java
+++ SingletonShapeFactory.java
@@ -7,6 +7,47 @@
 
 public class SingletonShapeFactory {
 
-	
+	private static SingletonShapeFactory instance = new SingletonShapeFactory();
+
+	/**
+	 * Constructor
+	 * 
+	 * It's private so that it cannot be instantiated
+	 */
+	private SingletonShapeFactory() {
+		System.out.println("Inside the shape factory constructor");
+	}
+
+	/**
+	 * Get's the available instance
+	 */
+	public static SingletonShapeFactory getInstance() {
+		return instance;
+	}
+
+	/**
+	 * Returns a Shape based on what's requested
+	 */
+	public Shape getShape(String shapeType) {
+
+		if (shapeType == null) {
+			return null;
+		}
+
+		if (shapeType.equalsIgnoreCase("CIRCLE")) {
+			return new Circle();
+		}
+
+		if (shapeType.equalsIgnoreCase("RECTANGLE")) {
+			return new Rectangle();
+		}
+
+		if (shapeType.equalsIgnoreCase("SQUARE")) {
+			return new Square();
+		}
+
+		return null;
+	}
+
 
 }

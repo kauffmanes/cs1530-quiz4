166ecbf0b0f74b9b75d92e9e1cc3640c60f2b536
Emily Kauffman <kauffmanes@gmail.com>
2019-07-16 20:31:52 -0400

    printing

--- AbstractFactoryTutorial.java
+++ AbstractFactoryTutorial.java
@@ -9,31 +9,41 @@ public class AbstractFactoryTutorial {
 
 	public static void main(String[] args) {
 
-		System.out.println("New set:");
+		// does it a few times to demonstrate the IDs not changing
+		for (int loop = 1; loop < 3; loop++) {
 
-		// DRAWS A CIRCLE SHAPE
-		AbstractFactory factory1 = FactoryProducer.getFactory("SHAPE");
-		factory1.showMessage(); // Shape Factory ID: 5972
-		Shape circle = factory1.getShape("CIRCLE"); // 
-		circle.draw(); // Circle ID: 1879
+			System.out.println("New set:");
 
-		// DRAWS A RECTANGLE SHAPE
-		AbstractFactory factory2 = FactoryProducer.getFactory("SHAPE");
-		factory2.showMessage(); // Shape Factory ID: 5972
-		Shape rectangle = factory2.getShape("RECTANGLE");
-		rectangle.draw(); // Rectangle ID: 823
+			// DRAWS A CIRCLE SHAPE
+			AbstractFactory factory1 = FactoryProducer.getFactory("SHAPE");
+			factory1.showMessage(); // Shape Factory ID: 5972
+			Shape circle = factory1.getShape("CIRCLE"); // 
+			circle.draw(); // Circle ID: 1879
 
-		// DRAWS A RED COLOR
-		AbstractFactory factory3 = FactoryProducer.getFactory("COLOR");
-		factory3.showMessage(); // Color Factory ID: 2155
-		Color red = factory3.getColor("RED");
-		red.fill();
+			// DRAWS A RECTANGLE SHAPE
+			AbstractFactory factory2 = FactoryProducer.getFactory("SHAPE");
+			factory2.showMessage(); // Shape Factory ID: 5972
+			Shape rectangle = factory2.getShape("RECTANGLE");
+			rectangle.draw(); // Rectangle ID: 823
 
-		// DRAWS A GREEN COLOR
-		AbstractFactory factory4 = FactoryProducer.getFactory("COLOR");
-		factory4.showMessage(); // Color Factory ID: 2155
-		Color green = factory4.getColor("GREEN");
-		green.fill();
+			// DRAWS A RED COLOR
+			AbstractFactory factory3 = FactoryProducer.getFactory("COLOR");
+			factory3.showMessage(); // Color Factory ID: 2155
+			Color red = factory3.getColor("RED");
+			red.fill(); // RED ID: 5268
 
+			// DRAWS A GREEN COLOR
+			AbstractFactory factory4 = FactoryProducer.getFactory("COLOR");
+			factory4.showMessage(); // Color Factory ID: 2155
+			Color green = factory4.getColor("GREEN");
+			green.fill(); // GREEN ID: 6282
+
+			// DRAWS A BLUE COLOR
+			AbstractFactory factory5 = FactoryProducer.getFactory("COLOR");
+			factory5.showMessage(); // Color Factory ID: 2155
+			Color blue = factory5.getColor("BLUE");
+			blue.fill(); // BLUE ID: 8976
+
+		}
 	}
 }
